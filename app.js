bde48e219b74274ceced821c30d64da43ce5962b
Isaac Nadar <isaac.v.nadar@gmail.com>
2018-02-28 11:56:48 +0530

    style changes in layout, added the response data from api

--- app.js
+++ app.js
@@ -7,8 +7,14 @@ angular.module('app', [])
 //
 // Application controller.
 //
-.controller('AppCtrl', function AppCtrl ($scope) {
+.controller('AppCtrl', function AppCtrl ($scope, $http) {
 
+	$http.get("https://api.myjson.com/bins/1glgyd")
+    .then(function(response) {
+		$scope.myWelcome = response.data.leg_data;
+		console.log($scope.myWelcome);
+	});
+	
 	//
 	// Setup the application data-model.
 	//
